ada5f04eebd098e3f2a1c9d6bf47a77642de3014
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-25 11:59:49 -0300

    Profile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -12,6 +12,7 @@ import {
 } from 'lucide-react'
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
 
 export function Sidebar() {
   return (
@@ -42,6 +43,9 @@ export function Sidebar() {
         </nav>
 
         <UsedSpaceWidget />
+
+       <div className='h-px bg-zinc-300'/>
+       <Profile />
       </div>
     </aside>
   )
