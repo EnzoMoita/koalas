97d5cf3dc9af115c873a26a9fecd8ff0263ecbc3
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-25 12:26:32 -0300

    Add RadixUI for Tabs

--- src/components/Sidebar/UsedSpaceWidget.tsx
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -17,13 +17,13 @@ export function UsedSpaceWidget() {
         <div className="space-x-3">
           <button
             type="button"
-            className="text-sm font-medium text-red-500 hover:text-violet-700"
+            className="text-sm font-medium text-red-500 hover:text-red-700"
           >
             Fechar
           </button>
           <button
             type="button"
-            className="text-sm font-medium text-violet-700 hover:text-violet-900"
+            className="text-sm font-medium text-violet-700 hover:text-green-500"
           >
             Atualizar plano
           </button>
